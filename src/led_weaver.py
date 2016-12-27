f688b76d98b61d38db87c0034e64de0338ab02cc
Vu Phan <vuphan314@gmail.com>
2016-12-27 17:02:13 +0700

    magicpython

--- src/led_weaver.py
+++ src/led_weaver.py
@@ -3,6 +3,16 @@
 ############################################################
 
 def weave_top(T: tuple) -> str:
+    st = weave_recur(T)
+    return st
+
+def weave_recur(T: tuple) -> str:
     pass
 
 ############################################################
+
+"""
+T[0] == 'constDef'
+
+st ==
+"""
