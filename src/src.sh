bec5d378a06ad9aeffa5a8b43084f07351130bf6
Vu Phan <vuphan314@gmail.com>
2017-01-04 16:35:19 +0700

    TeX comment

--- src/src.sh
+++ src/src.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-input_base=tmp
-input_base=tictactoe
+input_base=comment
 
 examples_path=../examples
 base_path=$examples_path/$input_base
@@ -10,6 +9,5 @@ tex_path=$base_path.tex
 out_path=$examples_path
 
 # python3 led_parser.py $led_path
-# python3 led_engine.py -f $led_path
-# atom $tex_path
+python3 led_engine.py -f $led_path
 latexmk -pdf -outdir=$out_path $base_path
