8378bf51f95c2dfa34ca51bba36572795863eb96
Vu Phan <vuphan314@gmail.com>
2017-01-07 22:40:35 +0700

    medskip noindent

--- src/src.sh
+++ src/src.sh
@@ -1,4 +1,4 @@
-input_base=precedence # precedence, tictactoe, tmp
+input_base=tmp # precedence, tictactoe, tmp
 
 examples_path=../examples
 base_path=$examples_path/$input_base
@@ -7,9 +7,9 @@ tex_path=$base_path.tex
 out_path=$examples_path
 pdf_path=$out_path/$base_path.pdf
 
+# atom $tex_path
+evince $pdf_path
+
 # python3 led_parser.py $led_path
 python3 led_engine.py -f $led_path
 latexmk -pdf -outdir=$out_path $base_path
-
-# atom $tex_path
-# evince $pdf_path
