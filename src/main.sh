93ad8a5242760e90b905a2014650cd44ab4bc138
Vu Phan <vuphan314@gmail.com>
2017-03-17 17:19:32 -0500

    brush up main.sh

--- src/main.sh
+++ src/main.sh
@@ -1,4 +1,4 @@
-input_base=precedence # precedence, tictactoe, tmp
+input_base=tmp # precedence, tictactoe, tmp
 
 examples_path=../examples
 base_path=$examples_path/$input_base
@@ -9,7 +9,7 @@ pdf_path=$out_path/$base_path.pdf
 
 # python3 led_parser.py $led_path
 python3 led_engine.py -f $led_path
-latexmk -pdf -outdir=$out_path $base_path
+# latexmk -pdf -outdir=$out_path $tex_path
 
-# atom $tex_path
-# evince $pdf_path
+# cat $tex_path
+# evince $pdf_path &
