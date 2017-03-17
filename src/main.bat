6683f76eeb7a8804f8329a13c97e3f4cda88e79c
Vu Phan <vuphan314@gmail.com>
2017-03-17 17:15:15 -0500

    2-space tab in .bat

--- src/main.bat
+++ src/main.bat
@@ -1,31 +1,31 @@
 goto starting
 
 :looping
-    set fold=..\examples\
-    set fil=tictactoe
-    set fils=aggregation, boolean, comparison, countingGame, definition, nonstrict, quantification, set, tictactoe, tmp, tmpGame
-    for %%i in (%fil%) do (
-        set base=%fold%%%~ni
-        set led=!base!.led
-        set p=!base!.p
-        set sl=!base!.sl
+  set fold=..\examples\
+  set fil=tictactoe
+  set fils=aggregation, boolean, comparison, countingGame, definition, nonstrict, quantification, set, tictactoe, tmp, tmpGame
+  for %%i in (%fil%) do (
+    set base=%fold%%%~ni
+    set led=!base!.led
+    set p=!base!.p
+    set sl=!base!.sl
 
-        set engine=led_engine.py !led!
+    set engine=led_engine.py !led!
 
-        REM !engine!
-        !engine! > !sl!
-        sli -l !sl!
-    )
-    goto ending
+    REM !engine!
+    !engine! > !sl!
+    sli -l !sl!
+  )
+  goto ending
 
 :starting
-    cls
-    @echo off
-    setlocal enabledelayedexpansion
-    echo starting...
-    echo:
-    goto looping
+  cls
+  @echo off
+  setlocal enabledelayedexpansion
+  echo starting...
+  echo:
+  goto looping
 
 :ending
-    echo:
-    echo done
+  echo:
+  echo done
