f0db91d543e53a80f4979b99031aa9a5917bd539
Vu <vuphan314@gmail.com>
2017-08-06 21:14:25 -0500

    main_demo.sh

--- src/demo/main_demo.sh
+++ src/demo/main_demo.sh
@@ -1,16 +1,37 @@
-input_base=$1 # dir `demo/`: tab-autocompletion appends `.`
-# aggregation, boolean, comment, comparison, countingGame, definition, nonstrict, precedence, quantification, set, tictactoe
+function set_paths {
+  base_path=$1 # with trailing `.`
 
-demo_path=.
-base_path=$demo_path/$input_base
-led_path=${base_path}led
-sl_path=${base_path}sl
-tex_path=${base_path}tex
-out_path=$demo_path
-pdf_path=$out_path/${base_path}pdf
+  led_path=${base_path}led
+  sl_path=${base_path}sl
+  tex_path=${base_path}tex
+  pdf_path=${base_path}pdf
+}
+
+function process_led_file {
+  led_path=$1
+
+  ../led $led_path -f -v
+  # atom $led_path $sl_path $tex_path
+  # latexmk -pdf $tex_path # -synctex=1
+  # evince $pdf_path &
+}
+
+function run_one {
+  base_path=$1
+
+  set_paths $base_path
+  process_led_file $led_path
+}
+
+function run_all {
+  base_paths=(aggregation. boolean. comment. comparison. countingGame. definition. nonstrict. precedence. quantification. set. tictactoe.)
+  for base_path in ${base_paths[@]}; do
+    set_paths $base_path
+    process_led_file $led_path
+  done
+}
 
 clear
-../led $led_path -f $2 # -v
-# atom $led_path $sl_path $tex_path
-# latexmk -pdf -outdir=$out_path $tex_path # -synctex=1
-# evince $pdf_path &
+run_one $1
+# run_all
+latexmk -c
